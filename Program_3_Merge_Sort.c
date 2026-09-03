b83a62cef8650f84c658989e028918e3decadca3
shradharevankar-15 <shradha15.clg@gmail.com>
2026-09-03 09:21:56 +0530

    Add merge sort implementation in Program_3_Merge_Sort.c
    Implement merge sort algorithm for sorting an array.

---
+++ Program_3_Merge_Sort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+void merge(int arr[], int low, int mid, int high) {
+    int temp[100];
+    int i = low;
+    int j = mid + 1;
+    int k = 0;
+
+    while (i <= mid && j <= high) {
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i <= mid)
+        temp[k++] = arr[i++];
+
+    while (j <= high)
+        temp[k++] = arr[j++];
+
+    for (i = low, k = 0; i <= high; i++, k++)
+        arr[i] = temp[k];
+}
+
+void mergeSort(int arr[], int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+
+        merge(arr, low, mid, high);
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements: ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
